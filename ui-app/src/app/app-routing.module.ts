dfca71f50d569a67f38e6bfd2daca7787596c1d5
sridharpedamallu <sridharpedamallu@gmail.com>
2020-11-30 20:39:55 +0530

    CR #1

--- ui-app/src/app/app-routing.module.ts
+++ ui-app/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }, { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }, { path: 'register', loadChildren: () => import('./register/register.module').then(m => m.RegisterModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
